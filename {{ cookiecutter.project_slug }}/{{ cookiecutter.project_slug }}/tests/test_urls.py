6a5840ed79f607b7d70eab80f4815799cb29eaff
Clinton Blackburn <clinton.blackburn@gmail.com>
2018-11-24 14:54:10 -0800

    Added /health endpoint
    This endpoint can be used to determine if the application is running.

--- {{ cookiecutter.project_slug }}/{{ cookiecutter.project_slug }}/tests/test_urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import reverse
-
-
-def test_admin(admin_client):
-    """ Sanity check to ensure Admin loads. """
-    response = admin_client.get(reverse('admin:index'))
-    assert response.status_code == 200
