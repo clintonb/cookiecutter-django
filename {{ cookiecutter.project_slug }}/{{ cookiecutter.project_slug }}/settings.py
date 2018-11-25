073d0f2765af1858b27bf4e5eadf7dcc2facc704
Clinton Blackburn <clinton.blackburn@gmail.com>
2018-11-25 11:17:24 -0800

    Using Argon2 for password hashing

--- {{ cookiecutter.project_slug }}/{{ cookiecutter.project_slug }}/settings.py
+++ {{ cookiecutter.project_slug }}/{{ cookiecutter.project_slug }}/settings.py
@@ -99,6 +99,12 @@ AUTH_PASSWORD_VALIDATORS = [
 
 AUTH_USER_MODEL = 'core.User'
 
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.Argon2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+]
+
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
