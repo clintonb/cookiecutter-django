f43f4289628254fb30c6bf2b1f367f30ffdf906f
Clinton Blackburn <clinton.blackburn@gmail.com>
2019-02-16 13:22:14 -0800

    Added Cookiecutter variables for Terraform state bucket

--- {{ cookiecutter.project_slug }}/terraform/terraform.tf
+++ {{ cookiecutter.project_slug }}/terraform/terraform.tf
@@ -1,7 +1,6 @@
 terraform {
   backend "s3" {
-    // TODO Add a prefix to the bucket name (e.g. com.example.myproject.terraform). Update backend_setup/main.tf, too!
-    bucket         = "<prefix>.{{ cookiecutter.project_slug }}.terraform"
+    bucket         = "{{ cookiecutter.terraform_backend_s3_bucket_name }}"
     key            = "terraform.tfstate"
     region         = "us-west-2"
     dynamodb_table = "terraform_{{ cookiecutter.project_slug }}"
